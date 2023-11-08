8dfdb7b471ea729c182f56b561afa946f06fd4de
dastu25 <dudekuladastagiri2002@gmail.com>
2023-11-08 16:42:24 +0530

    first commit

---
+++ app2/star=1.py
@@ -0,0 +1,6 @@
+star=1
+jy=0
+space=1
+for space in range(0,3,2):
+    print('*'*star,'-'*space,'*'*jy)
+    jy=1
---
+++ app2/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def Jampandu(request):
+    return HttpResponse ("hi jampandu how are u")
